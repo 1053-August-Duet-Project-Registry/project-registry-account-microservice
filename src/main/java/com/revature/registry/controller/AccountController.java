c5bc17349cff1d4b9ea34b593b5b252e4c9528ca
JW406 <jwvic.2357@gmail.com>
2021-08-08 18:48:33 -0400

    Add account retrieval endpoints.

--- src/main/java/com/revature/registry/controller/AccountController.java
+++ src/main/java/com/revature/registry/controller/AccountController.java
@@ -1,6 +1,5 @@
 package com.revature.registry.controller;
 
-import java.net.URI;
 import java.util.List;
 
 import com.revature.registry.model.Account;
@@ -10,13 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +18,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api/account", produces = MediaType.APPLICATION_JSON_VALUE)
 //@CrossOrigin(origins = "http://localhost:4200")
 public class AccountController {
-    
+    @Autowired
+    private AccountService accountService;
+    @GetMapping("")
+    public ResponseEntity<List<Account>> getAllAccounts() {
+        return new ResponseEntity<>(accountService.getAllAccounts(), HttpStatus.OK);
+    }
+
+    @GetMapping("/id/{id}")
+    public ResponseEntity<Account> getAllAccounts(@PathVariable("id") int id) {
+        Account account = accountService.getAccountByID(id);
+        if (account != null) {
+            return new ResponseEntity<>(account, HttpStatus.OK);
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
+    }
 }
